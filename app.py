67abc26c982cfdffeed058ba68c9de07716fe525
asimaarbi <asimfarooq5@gmail.com>
2020-12-08 15:37:25 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import shlex
+import subprocess
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/api/volume/<status>')
+def power_ops(status):
+    if status == 'mute':
+        stt = subprocess.check_call(shlex.split("amixer -D pulse sset Master status"))
+        print(stt)
+        return 'Muted'
+    elif status == 'unmute':
+        subprocess.check_call(shlex.split("amixer -D pulse sset Master unmute"))
+        return 'Unmuted'
+    else:
+        stat = subprocess.check_call(shlex.split("pacmd list-sinks | awk '/muted/ { print $2 }'"))
+        print(type(stat))
+        return status
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7778, debug=True)
