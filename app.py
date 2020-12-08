8b9c6145c36feb58642cbdcee85ff097ceb41e0f
asimaarbi <asimfarooq5@gmail.com>
2020-12-08 18:15:21 +0500

    fixes

--- app.py
+++ app.py
@@ -8,17 +8,14 @@ app = Flask(__name__)
 
 @app.route('/api/volume/<status>')
 def power_ops(status):
-    if status == 'mute':
-        stt = subprocess.check_call(shlex.split("amixer -D pulse sset Master mute"))
-        return 'Muted'
-    elif status == 'unmute':
-        subprocess.check_call(shlex.split("amixer -D pulse sset Master unmute"))
-        return 'Unmuted'
+    if status:
+        subprocess.check_call(shlex.split(f"amixer -D pulse sset Master {status}"))
+        return status
     else:
         raw = subprocess.check_output(shlex.split("pacmd list-sinks"))
         is_muted = raw.find("muted: yes") != -1
-        return is_muted
+        return str(is_muted)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7778, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
