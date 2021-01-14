268c286cd302306f17d6ad38d360f92c2f2c04e1
asimaarbi <asimfarooq5@gmail.com>
2021-01-14 15:10:22 +0500

    pesh_service

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
+import os
 import shlex
 import subprocess
 
 from flask import Flask
 
+from lock_screen import Display
+from pesh_service import Discovery
+
 app = Flask(__name__)
+display = Display()
+discovery = Discovery(9000)
 
 
 @app.route('/api/volume/<status>')
 def volume_ops(status):
     if status == 'status':
+        print("yhn hoon main2")
         raw = subprocess.check_output(shlex.split("pacmd list-sinks"))
         is_muted = raw.find(b"muted: yes") != -1
         return f'{is_muted}'
@@ -22,5 +29,11 @@ def volume_ops(status):
     return status
 
 
+@app.route('/api/lock/')
+def lock():
+    return str(True if display.is_locked() else False), 200
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    discovery.publish()
+    app.run(host='0.0.0.0', port=5009, debug=True)
